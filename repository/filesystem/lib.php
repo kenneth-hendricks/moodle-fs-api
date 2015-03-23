9671c393fd393e0bc15dddfee90c277df0053cad
Andrew Nicols <andrew@nicols.co.uk>
2015-03-23 09:13:26 +0800

    MDL-49627 core: Update uses of generate_image_thumbnail
    This removes the need to write tempfiles in these locations.

--- repository/filesystem/lib.php
+++ repository/filesystem/lib.php
@@ -481,7 +481,6 @@ class repository_filesystem extends repository {
             return null;
         }
         $filename = sha1($filecontents);
-        unset($filecontents);
 
         // Try to get generated thumbnail for this file.
         $fs = get_file_storage();
@@ -494,7 +493,7 @@ class repository_filesystem extends repository {
             } else {
                 $size = 24;
             }
-            if (!$data = @generate_image_thumbnail($origfile, $size, $size)) {
+            if (!$data = generate_image_thumbnail_from_string($filecontents, $size, $size)) {
                 // Generation failed.
                 return null;
             }
